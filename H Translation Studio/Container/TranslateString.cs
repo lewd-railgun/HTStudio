4ef65623d16415e98466d62282a0c45c86f9b3e5
Lewd Railgun <lewd.railgun@gmail.com>
2019-03-25 22:49:43 +0900

    번역 가능한 문자열을 저장하는 클래스 추가

---
+++ H Translation Studio/Container/TranslateString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HTStudio.Container
+{
+    //번역 가능한 문자열
+    public class TranslateString
+    {
+        public string Original, Machine, Hand;
+    }
+}
