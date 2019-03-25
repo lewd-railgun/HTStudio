2b4a04197b49c16b125e91cbb4b1c30c0a84ca15
Lewd Railgun <lewd.railgun@gmail.com>
2019-03-25 20:40:18 +0900

    StartWindow 추가

---
+++ H Translation Studio/Views/StartWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HTStudio.Views
+{
+    /// <summary>
+    /// StartPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.FileVersion;
+            VersionTextBlock.Text = "Version " + version;
+        }
+
+        private void NewButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: Select Project Directory
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: Open Last Project
+            MessageBox.Show("최근에 열었던 프로젝트가 없습니다");
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+    }
+}
